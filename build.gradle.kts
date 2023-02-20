f64efe83e818228be43573ef810baa696a783bf3
Hleb Haliuk <HHaliuk@ibagroup.eu>
2023-02-20 18:37:57 +0100

    Changed org.jebrains.intellij version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
 
 plugins {
   id("org.sonarqube") version "3.3"
-  id("org.jetbrains.intellij") version "1.9.0"
+  id("org.jetbrains.intellij") version "1.13.0"
   kotlin("jvm") version "1.6.21"
   java
   jacoco

aa79cde32444b14f3677d8918e5513058c08b174
Hleb Haliuk <HHaliuk@ibagroup.eu>
2023-11-16 16:50:47 +0100

    Changed path of plugin-verifier

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -99,7 +99,7 @@ pipeline {
                     resultFileName = sh(returnStdout: true, script: "cd build/distributions/ && ls").trim()
                 }
                 sh """
-                java -jar /root/plugin-verifier/verifier-all.jar check-plugin build/distributions/$resultFileName
+                java -jar /plugin-verifier/verifier-all.jar check-plugin build/distributions/$resultFileName
                 """
             }
         }

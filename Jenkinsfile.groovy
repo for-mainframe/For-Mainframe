a29945117bc11fc308321bde98fb934be19fb6fb
Uladzislau <leksilonchikk@gmail.com>
2024-03-27 21:48:34 +0100

    IJMP-1424 Another publishing check
    Signed-off-by: Uladzislau <leksilonchikk@gmail.com>

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -49,123 +49,128 @@ pipeline {
         jdk 'Java 11'
     }
     stages {
-        stage('Initial checkup') {
+        stage('Test check') {
             steps {
-                sh 'java -version'
+                echo INTELLIJ_SIGNING_CERTIFICATE_CHAIN
             }
         }
-        stage('Get Jira Ticket') {
-            steps {
-                echo gitlabBranch
-                script {
-                    if (gitlabBranch.equals("development")) {
-                        jiraTicket = 'development'
-                    } else if (gitlabBranch.equals("zowe-development")) {
-                        jiraTicket = 'zowe-development'
-                    } else if (gitlabBranch.contains("release")) {
-                        jiraTicket = gitlabBranch
-                    } else {
-                        def pattern = ~/(?i)ijmp-\d+/
-                        def matcher = gitlabBranch =~ pattern
-                        if (matcher.find()) {
-                            jiraTicket = matcher[0].toUpperCase()
-                        } else {
-                            jiraTicket = "null"
-                            echo "Jira ticket name wasn't found!"
-                        }
-                    }
-                }
-                echo "Jira ticket: $jiraTicket"
-            }
-        }
-        stage('Clone Branch') {
-            steps {
-                cleanWs()
-                sh "ls -la"
-                git branch: "$gitlabBranch", credentialsId: "$gitCredentialsId", url: "$gitUrl"
-            }
-        }
-        stage('Build Plugin') {
-            steps {
-                // sh 'sudo chmod +x /etc/profile.d/gradle.sh'
-                // sh 'sudo -s source /etc/profile.d/gradle.sh'
-                withGradle {
-                    // To change Gradle version - Jenkins/Manage Jenkins/Global Tool Configuration
-                    // sh 'gradle -v'
-                    sh 'gradle wrapper'
-                    sh './gradlew -v'
-                    // sh './gradlew test'
-                    // sh './gradlew buildPlugin'
-                }
-            }
-        }
-        stage('Verify Plugin') {
-            steps {
-                withGradle {
-                    script {
-                        if (gitlabBranch.contains("release")) {
-                            sh './gradlew runPluginVerifier'
-                        } else {
-                            echo 'Plugin verification is skipped as the branch to verify is not a release branch'
-                        }
-                    }
-                }
-            }
-        }
-        stage('Release Plugin') {
-            steps {
-                withGradle {
-                    script {
-                        // if (gitlabBranch.contains("release-publish")) {
-                        if (gitlabBranch.contains("feature/IJMP-1401-plugin-verifier-jenkins")) {
-                            sh './gradlew publishPlugin'
-                        } else {
-                            echo 'Does not publish the version as the branch is not the "release-publish" branch'
-                        }
-                    }
-                }
-            }
-        }
-        stage('Form and post Jira message') {
-            steps {
-                script {
-                    resultFileName = sh(returnStdout: true, script: "cd build/distributions/ && ls").trim()
-                }
-                sh """
-                if [ "$jiraTicket" = "null" ]
-                then
-                    echo "jira ticket is not determined"
-                else
-                    if [ -d "/var/www/ijmp-plugin/$jiraTicket" ]
-                    then
-                        sudo rm -r /var/www/ijmp-plugin/$jiraTicket
-                    fi
-                    sudo mkdir -p /var/www/ijmp-plugin/$jiraTicket
-                    sudo mkdir /var/www/ijmp-plugin/$jiraTicket/idea
-                    sudo mkdir /var/www/ijmp-plugin/$jiraTicket/pycharm
+        // stage('Initial checkup') {
+        //     steps {
+        //         sh 'java -version'
+        //     }
+        // }
+        // stage('Get Jira Ticket') {
+        //     steps {
+        //         echo gitlabBranch
+        //         script {
+        //             if (gitlabBranch.equals("development")) {
+        //                 jiraTicket = 'development'
+        //             } else if (gitlabBranch.equals("zowe-development")) {
+        //                 jiraTicket = 'zowe-development'
+        //             } else if (gitlabBranch.contains("release")) {
+        //                 jiraTicket = gitlabBranch
+        //             } else {
+        //                 def pattern = ~/(?i)ijmp-\d+/
+        //                 def matcher = gitlabBranch =~ pattern
+        //                 if (matcher.find()) {
+        //                     jiraTicket = matcher[0].toUpperCase()
+        //                 } else {
+        //                     jiraTicket = "null"
+        //                     echo "Jira ticket name wasn't found!"
+        //                 }
+        //             }
+        //         }
+        //         echo "Jira ticket: $jiraTicket"
+        //     }
+        // }
+        // stage('Clone Branch') {
+        //     steps {
+        //         cleanWs()
+        //         sh "ls -la"
+        //         git branch: "$gitlabBranch", credentialsId: "$gitCredentialsId", url: "$gitUrl"
+        //     }
+        // }
+        // stage('Build Plugin') {
+        //     steps {
+        //         // sh 'sudo chmod +x /etc/profile.d/gradle.sh'
+        //         // sh 'sudo -s source /etc/profile.d/gradle.sh'
+        //         withGradle {
+        //             // To change Gradle version - Jenkins/Manage Jenkins/Global Tool Configuration
+        //             // sh 'gradle -v'
+        //             sh 'gradle wrapper'
+        //             sh './gradlew -v'
+        //             // sh './gradlew test'
+        //             // sh './gradlew buildPlugin'
+        //         }
+        //     }
+        // }
+        // stage('Verify Plugin') {
+        //     steps {
+        //         withGradle {
+        //             script {
+        //                 if (gitlabBranch.contains("release")) {
+        //                     sh './gradlew runPluginVerifier'
+        //                 } else {
+        //                     echo 'Plugin verification is skipped as the branch to verify is not a release branch'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // stage('Release Plugin') {
+        //     steps {
+        //         withGradle {
+        //             script {
+        //                 // if (gitlabBranch.contains("release-publish")) {
+        //                 if (gitlabBranch.contains("feature/IJMP-1401-plugin-verifier-jenkins")) {
+        //                     sh './gradlew publishPlugin'
+        //                 } else {
+        //                     echo 'Does not publish the version as the branch is not the "release-publish" branch'
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // stage('Form and post Jira message') {
+        //     steps {
+        //         script {
+        //             resultFileName = sh(returnStdout: true, script: "cd build/distributions/ && ls").trim()
+        //         }
+        //         sh """
+        //         if [ "$jiraTicket" = "null" ]
+        //         then
+        //             echo "jira ticket is not determined"
+        //         else
+        //             if [ -d "/var/www/ijmp-plugin/$jiraTicket" ]
+        //             then
+        //                 sudo rm -r /var/www/ijmp-plugin/$jiraTicket
+        //             fi
+        //             sudo mkdir -p /var/www/ijmp-plugin/$jiraTicket
+        //             sudo mkdir /var/www/ijmp-plugin/$jiraTicket/idea
+        //             sudo mkdir /var/www/ijmp-plugin/$jiraTicket/pycharm
 
-                    sudo mv build/distributions/$resultFileName /var/www/ijmp-plugin/$jiraTicket/idea
-                fi
-                """
-            }
-            post {
-                success {
-                    script {
-                        if (!jiraTicket.contains('release') && !'development'.equals(jiraTicket) && !'zowe-development'.equals(jiraTicket) && !"null".equals(jiraTicket)) {
-                            jiraAddComment idOrKey: "$jiraTicket", comment: "Hello! It's jenkins. Your push in branch was successfully built. You can download your build from the following link $apacheInternalUrl/ijmp-plugin/$jiraTicket/idea/$resultFileName.", site: "$jiraSite"
-                        }
+        //             sudo mv build/distributions/$resultFileName /var/www/ijmp-plugin/$jiraTicket/idea
+        //         fi
+        //         """
+        //     }
+        //     post {
+        //         success {
+        //             script {
+        //                 if (!jiraTicket.contains('release') && !'development'.equals(jiraTicket) && !'zowe-development'.equals(jiraTicket) && !"null".equals(jiraTicket)) {
+        //                     jiraAddComment idOrKey: "$jiraTicket", comment: "Hello! It's jenkins. Your push in branch was successfully built. You can download your build from the following link $apacheInternalUrl/ijmp-plugin/$jiraTicket/idea/$resultFileName.", site: "$jiraSite"
+        //                 }
 
-                    }
-                }
-                failure {
-                    script {
-                        if (!jiraTicket.contains('release') && !'development'.equals(jiraTicket) && !'zowe-development'.equals(jiraTicket) && !"null".equals(jiraTicket)) {
-                            jiraAddComment idOrKey: "$jiraTicket", comment: "Hello! It's jenkins. Your push in branch failed to build for Intellij IDEA. You can get console output by the following link $jenkinsServerUrl/job/BuildPluginPipeline/", site: "$jiraSite"
-                        }
-                    }
-                }
-            }
-        }
+        //             }
+        //         }
+        //         failure {
+        //             script {
+        //                 if (!jiraTicket.contains('release') && !'development'.equals(jiraTicket) && !'zowe-development'.equals(jiraTicket) && !"null".equals(jiraTicket)) {
+        //                     jiraAddComment idOrKey: "$jiraTicket", comment: "Hello! It's jenkins. Your push in branch failed to build for Intellij IDEA. You can get console output by the following link $jenkinsServerUrl/job/BuildPluginPipeline/", site: "$jiraSite"
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
         // stage('Change Plugin Version'){
         //     steps{
